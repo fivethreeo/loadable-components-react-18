9cd8bb4b452db6801c6976ae7181d5ce23e92ea4
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2022-11-14 23:59:59 +0100

    working css config

--- config/css.js
+++ config/css.js
@@ -13,74 +13,86 @@ const postCssOptions = {
 const config = {
     rules: {
         prod: {
-            server: [
-                {
-                    loader: "css-loader",
-                    options: {
-                        importLoaders: 1,
-                        modules: {
-                            auto: true,
-                            exportOnlyLocals: true,
-                            localIdentName: "[name]__[local]___[hash:base64:5]"
-                        },
-                    }
-                }],
+            server: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                auto: true,
+                                exportOnlyLocals: true,
+                                localIdentName: "[name]__[local]___[hash:base64:5]"
+                            },
+                        }
+                    }]
+            }],
 
-            client: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                {
-                    loader: "css-loader",
-                    options: {
-                        sourceMap: false,
-                        importLoaders: 1,
-                        modules: {
-                            auto: true,
-                            localIdentName: "[name]__[local]___[hash:base64:5]",
-                        },
-                    }
-                },
-                {
-                    loader: "postcss-loader",
-                    options: postCssOptions,
-                }]
+            client: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: false,
+                            importLoaders: 1,
+                            modules: {
+                                auto: true,
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: postCssOptions,
+                    }]
+            }],
         },
 
 
         dev: {
-            server: [
-                {
-                    loader: "css-loader",
-                    options: {
-                        importLoaders: 1,
-                        modules: {
-                            auto: true,
-                            exportOnlyLocals: true,
-                            localIdentName: "[name]__[local]___[hash:base64:5]"
-                        },
-                    }
-                }],
+            server: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                auto: true,
+                                exportOnlyLocals: true,
+                                localIdentName: "[name]__[local]___[hash:base64:5]"
+                            },
+                        }
+                    }]
+            }],
 
-            client: [
-                {
-                    loader: "style-loader",
-                },
-                {
-                    loader: "css-loader",
-                    options: {
-                        sourceMap: true,
-                        importLoaders: 1,
-                        modules: {
-                            auto: true,
-                            localIdentName: "[name]__[local]___[hash:base64:5]",
-                        },
-                    }
-                },
-                {
-                    loader: "postcss-loader",
-                    options: { postcssOptions: postCssOptions },
-                }]
+            client: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1,
+                            modules: {
+                                auto: true,
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: { postcssOptions: postCssOptions },
+                    }]
+            }],
         }
     },
     plugins: {
