9fa8a99b6de8ca30fde3c482ceceed6c8685f5a1
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2022-11-15 19:56:53 +0100

    Working wrapped writeable

--- server/render.js
+++ server/render.js
@@ -1,15 +1,80 @@
 import React from 'react';
-import { renderToStream } from 'react-streaming/server'
-import { Helmet } from 'react-helmet';
+import { renderToPipeableStream } from 'react-dom/server';
+import { Writable } from 'stream';
 import App from '../src/components/App';
+import Html from './html.jsx';
 
 export default async (req, res, next) => {
-  const helmet = Helmet.renderStatic();
-  const {
-    injectToStream,
-    pipe, // Defined if running in Node.js, otherwise `null`
-    readable // Defined if running on the Edge (.e.g. Coudflare Workers), otherwise `null`
-  } = await renderToStream(<App />)
-  
-  pipe(res);
+  let didError = false;
+  let shellReady = false;
+
+  class LoadableWritable extends Writable {
+    constructor(writable) {
+      super();
+      this._writable = writable;
+    }
+
+
+    _write(chunk, encoding, callback) {
+      // This should pick up any new link tags that hasn't been previously
+      // written to this stream.
+      /*     let scriptTags = generateNewScriptTagsSinceLastCall();
+          if (scriptTags) {
+            // Write it before the HTML to ensure that we can start
+            // downloading it as early as possible.
+            this._writable.write(scriptTags);
+          } */
+      // Finally write whatever React tried to write.
+      if (shellReady) {
+        this._writable.write("ggg");
+      }
+
+      this._writable.write(chunk, encoding, callback);
+    }
+
+    end() {
+      this._writable.end()
+    }
+
+    flush() {
+      if (typeof this._writable.flush === 'function') {
+        this._writable.flush();
+      }
+    }
+  }
+  const writeable = new LoadableWritable(res)
+  const stream = renderToPipeableStream(<Html assets={{}}><App /></Html>,
+    {
+      onShellReady() {
+        // The content above all Suspense boundaries is ready.
+        // If something errored before we started streaming, we set the error code appropriately.
+        res.statusCode = didError ? 500 : 200;
+        res.setHeader('Content-type', 'text/html');
+        shellReady = true;
+
+        stream.pipe(writeable);
+
+      },
+      onShellError(error) {
+        // Something errored before we could complete the shell so we emit an alternative shell.
+        res.statusCode = 500;
+        res.send(
+          '<!doctype html><p>Loading...</p><script src="clientrender.js"></script>'
+        );
+      },
+      onAllReady() {
+        // If you don't want streaming, use this instead of onShellReady.
+        // This will fire after the entire page content is ready.
+        // You can use this for crawlers or static generation.
+
+        // res.statusCode = didError ? 500 : 200;
+        // res.setHeader('Content-type', 'text/html');
+        // stream.pipe(res);
+      },
+      onError(err) {
+        didError = true;
+        console.error(err);
+      },
+    }
+  );
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Helmet } from "react-helmet";
 
 
 export default () => (
   <div>
-    <Helmet>
-      <title>SSR MF Example</title>
-    </Helmet>
     <div style={{
       backgroundColor: 'black',
       color: 'lightgrey',
@@ -15,8 +11,5 @@ export default () => (
      <h1>Module Federation Example: Server Side Rendering</h1>
      <h2>This is the shell application.</h2>
     </div>
-   
-    <React.Suspense fallback={<h1>Loading....</h1>}>
-    </React.Suspense>
   </div >
 );
