d447a643e962e6eb90ee791ee03196c30626e02e
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2022-11-16 00:31:22 +0100

    It says loading so no suspense

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import loadable from '@loadable/component'
-
-const OtherComponent = loadable(() => import('./OtherComponent'))
+const OtherComponent = loadable(() => import('./OtherComponent'), { suspense: true})
 
 export default () => (
   <div>
@@ -13,6 +12,8 @@ export default () => (
      <h1>Module Federation Example: Server Side Rendering</h1>
      <h2>This is the shell application.</h2>
     </div>
+    <React.Suspense fallback="Loading">
       <OtherComponent />
+    </React.Suspense>
   </div >
 );
